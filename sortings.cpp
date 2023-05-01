1c2055f90b111472438d549812385ecc2696ae60
Anthony <aalbane6@binghamton.edu>
2023-04-30 20:07:24 -0400

    MergeSort

--- sortings.cpp
+++ sortings.cpp
@@ -82,6 +82,52 @@ void quickSort(int arr[], int low, int high){ //O(n^2) but usually O(nlogn)
     }
 }
 
+void merge(int arr[], int low, int middle, int high){
+    int x = 0;
+    int y = 0;
+    int z = low; //0 on first iteration
+
+    int sizeSub1 = middle - low + 1;
+    int sizeSub2 = high - middle;
+
+    int sub1[sizeSub1];
+    int sub2[sizeSub2];
+
+    for(int i = 0; i < sizeSub1; i++){
+        sub1[i] = arr[low+i];
+    }
+    for(int j = 0; j < sizeSub2; j++){
+        sub2[j] = arr[middle+1+j];
+    } //Initializing SubArray Data
+
+    while(x < sizeSub1 && y < sizeSub2){ //Begin to prepare to merge SubArrays
+        if(sub1[x] <= sub2[y]){arr[z] = sub1[x]; x++;}
+        else{arr[z] = sub2[y]; y++;}
+        z++;
+    }   
+
+    while(x < sizeSub1){ //Remnant Merge from sub1
+        arr[z] = sub1[x];
+        x++;
+        z++;
+    }
+
+    while(y < sizeSub2){ //Remnant Merge from sub2
+        arr[z] = sub2[y];
+        y++;
+        z++;
+    }
+
+}
+
+void mergeSort(int arr[], int low, int high){ //O(nlogn) guarantee
+    if(low < high){
+        int middle = (low + high) / 2;
+        mergeSort(arr, low, middle);
+        mergeSort(arr, middle + 1, high);
+        merge(arr, low, middle, high);
+    }
+}
 
 
 int main(int argc, char * argv[]){
@@ -90,7 +136,7 @@ int main(int argc, char * argv[]){
     int arr[4] = {3,2,1,4};
 
     printArr(arr, 4);
-    quickSort(arr, 0, 3);
+    mergeSort(arr, 0, 3);
     printArr(arr, 4);
 
 
