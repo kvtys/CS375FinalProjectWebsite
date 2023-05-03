9f10017c64cab39a3532f0ef87a13e8c42cdad13
kittysdotexe <akunnat1@binghamton.edu>
2023-05-02 20:32:08 -0400

    the algo's + website work woohoo!

--- SortAlgoWeb/src/app/app.component.ts
+++ SortAlgoWeb/src/app/app.component.ts
@@ -18,19 +18,24 @@ export class AppComponent {
   }
   
 
-  swap(arr: number[], i: number, j: number): void {
+  swap(arr: number[], i: number, j: number): number[] {
       const temp = arr[i];
       arr[i] = arr[j];
       arr[j] = temp;
+
+      return arr;
   }
 
-  selectionSort(arr: number[]): number[] {
+  async selectionSort(arr: number[]): Promise<number[]>{
       const size = arr.length;
-
+      let c = 0;
       for (let i = 0; i < size - 1; i++) {
           for (let j = i + 1; j < size; j++) {
-              if (arr[i] > arr[j]) {
-                  this.swap(arr, i, j);
+              if (arr[i] > arr[j]) { 
+                this.swap(arr, i, j);
+                this.step = `Step ${c + 1}: Swap ${arr[i]} and ${arr[j]} to get ${arr.join(', ')}`; //this will show up on the website itself, showing intermediate steps 
+                await this.delay(2000);
+                c++;
               }
           }
       }
@@ -60,31 +65,35 @@ export class AppComponent {
   }
   
   async bubbleSort(arr: number[]): Promise<number[]> {
-    console.log("this is arr" + arr.toString());
+    console.log("this is the starting arr " + arr.toString());
     const n = arr.length;
     let swapped: boolean;
-  
+    
+    let c = 0
     for (let i = 0; i < n - 1; i++) {
       swapped = false;
   
       for (let j = 0; j < n - 1 - i; j++) {
         if (arr[j] > arr[j + 1]) {
           // Swap elements
-          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+          console.log("the number " + arr[j].toString() + " is greater than the number " + arr[j+1].toString());
+
+          this.swap(arr, j, j+1);
           swapped = true;
-          this.step = `Step ${i + 1}: ${arr.join(', ')}`;
+          this.step = `Step ${c + 1}: Swap ${arr[j+1]} and ${arr[j]} to get ${arr.join(', ')}`; //this will show up on the website itself, showing intermediate steps 
+          await this.delay(2000);
+          c++;
         }
       }
-  
-      // Wait for 1 second
-      await this.delay(1000);
   
       // If no two elements were swapped in the inner loop, the array is already sorted
       if (!swapped) {
         break;
       }
+
     }
-    console.log("returning");
+
+    console.log("this is the end arr: " + arr);
     return arr;
   }
 
@@ -153,8 +162,23 @@ export class AppComponent {
   chooseAlgo(message: {arr: any, algo: number}): number[] {
     if(message.arr != ""){
       this.newArr = message.arr.split(",");
+
+      //THIS IS FOR DEBUGGING - AMEEN
+
+      for(let i = 0; i < this.newArr.length; i++ ){
+        console.log(this.newArr[i]);
+        if(this.newArr[i] < this.newArr[i+1]){
+          console.log(this.newArr[i].toString() + " < " + this.newArr[i+1].toString());
+        }
+      }
+
+      //DEBUGGING END
+
       if(message.algo == 1){
-        console.log(this.bubbleSort(this.newArr));
+        console.log(this.bubbleSort(this.newArr)); 
+      }
+      else if(message.algo == 2){
+        console.log(this.selectionSort(this.newArr));
       }
     }
     else{
